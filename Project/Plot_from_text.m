6c2d3d7a5174d6e565eec85393cc4f627f9317d7
DaniSode <danisode@student.chalmers.se>
2023-10-22 00:46:29 +0200

    Changes

--- Project/Plot_from_text.m
+++ Project/Plot_from_text.m
@@ -6,11 +6,13 @@ print_every = 25;
 %% Byt .txt fil här -->
 fileName_seg = 'loss_seg.txt';
 fileName_autoencoder = 'loss_autoencoder.txt';
-fileName_color_seg = 'loss_color_seg.txt';
+fileName_color_seg_true = 'loss_color_seg.txt';
+fileName_color_seg = 'loss_color_seg_full.txt';
 
 %% Detta blir titeln på plot samt filnamnet på .eps filen
 title_of_plot_seg = "Segmentation loss";
 title_of_plot_autoencoder = "Autoencoder loss";
+title_of_plot_color_seg_true = "Autoencoder using true segmentation loss";
 title_of_plot_color_seg = "Autoencoder using segmentation loss";
 title_of_plot_train_loss = "Train losses with and without segmentation";
 title_of_plot_val_loss = "Validation losses with and without segmentation";
@@ -18,6 +20,7 @@ title_of_plot_val_loss = "Validation losses with and without segmentation";
 %% All data från filerna
 [batch_vector_seg, train_loss_seg, val_loss_seg] = txt_to_data(fileName_seg, print_every);
 [batch_vector_autoencoder, train_loss_autoencoder, val_loss_autoencoder] = txt_to_data(fileName_autoencoder, print_every);
+[batch_vector_color_seg_true, train_loss_color_seg_true, val_loss_color_seg_true] = txt_to_data(fileName_color_seg_true, print_every);
 [batch_vector_color_seg, train_loss_color_seg, val_loss_color_seg] = txt_to_data(fileName_color_seg, print_every);
 
 %% Plot seg
@@ -45,6 +48,18 @@ ylim([0 250])
 legend("Validation loss", "Train loss")
 saveas(gcf, title_of_plot_autoencoder, 'epsc')
 
+%% Plot color seg true
+figure
+hold on; grid on; box on
+title(title_of_plot_color_seg_true)
+xlabel("Batches")
+ylabel("MSE Loss")
+set(gcf,'color','w');
+plot(batch_vector_color_seg_true, val_loss_color_seg_true,'LineWidth',1.5)
+plot(batch_vector_color_seg_true, train_loss_color_seg_true,'LineWidth',1.5)
+legend("Validation loss", "Train loss")
+saveas(gcf, title_of_plot_color_seg_true, 'epsc')
+
 %% Plot color seg
 figure
 hold on; grid on; box on
@@ -56,7 +71,6 @@ plot(batch_vector_color_seg, val_loss_color_seg,'LineWidth',1.5)
 plot(batch_vector_color_seg, train_loss_color_seg,'LineWidth',1.5)
 legend("Validation loss", "Train loss")
 saveas(gcf, title_of_plot_color_seg, 'epsc')
-
 
 %% Plot train vs train
 figure
